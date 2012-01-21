89ed598c102a524378479d0d911ea1e24c9fc0ae
Marcel Good <marcelg@ideablade.com>
2012-01-21 00:31:28 +0000

    Added dialog host custom ViewModel and View.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/DEV_Jeff@95 3d6527be-44ab-9e4e-9680-169f2067cef8

---
+++ Samples/TempHire/TempHire/ViewModels/Dialog/CustomDialogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Cocktail;
+
+namespace TempHire.ViewModels.Dialog
+{
+    /// <summary>
+    /// ViewModel to customize dialog and message boxes
+    /// </summary>
+    public class CustomDialogViewModel : DialogHostBase
+    {
+    }
+}

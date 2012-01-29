44a4ee8bcdd698e5f555244a40ec765c2cedf33f
Marcel Good <marcelg@ideablade.com>
2012-01-29 23:49:31 +0000

    Added ContinueOnError extension method.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@194 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Test Projects/Cocktail.Tests/OperationTests.cs
+++ Test Projects/Cocktail.Tests/OperationTests.cs
@@ -1,4 +1,6 @@
-﻿using Cocktail.Tests.Helpers;
+﻿using System;
+using Cocktail.Tests.Helpers;
+using IdeaBlade.EntityModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Cocktail.Tests
@@ -15,6 +17,41 @@ namespace Cocktail.Tests
             Assert.IsNull(operation.Error);
             Assert.IsFalse(operation.Cancelled);
             Assert.IsFalse(operation.IsErrorHandled);
+        }
+
+        [TestMethod]
+        public void ShouldMarkErrorAsHandled()
+        {
+            var operation = new CompleteWithError().AsOperationResult().ContinueOnError();
+
+            Assert.IsFalse(operation.CompletedSuccessfully);
+            Assert.IsTrue(operation.HasError);
+            Assert.IsNotNull(operation.Error);
+            Assert.IsFalse(operation.Cancelled);
+            Assert.IsTrue(operation.IsErrorHandled);
+        }
+
+        private class CompleteWithError : INotifyCompleted
+        {
+            private readonly INotifyCompletedArgs _args = new CompleteWithErrorArgs();
+
+            public void WhenCompleted(Action<INotifyCompletedArgs> completedAction)
+            {
+                completedAction(_args);
+            }
+
+            private class CompleteWithErrorArgs : INotifyCompletedArgs
+            {
+                public CompleteWithErrorArgs()
+                {
+                    Error = new Exception();
+                    Cancelled = false;
+                }
+
+                public Exception Error { get; private set; }
+                public bool Cancelled { get; private set; }
+                public bool IsErrorHandled { get; set; }
+            }
         }
     }
 }

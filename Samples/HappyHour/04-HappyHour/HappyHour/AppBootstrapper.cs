5a6990007ecaa3537fafdadac7009bbcfec6a487
Marcel Good <marcelg@ideablade.com>
2012-01-31 03:38:06 +0000

    Original Lesson 4 now shows the ItemTemplate approach. Lesson 5 shows the View-as-UserControl that was Lesson 4
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/DEV_Ward@211 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Samples/HappyHour/04-HappyHour/HappyHour/AppBootstrapper.cs
+++ Samples/HappyHour/04-HappyHour/HappyHour/AppBootstrapper.cs
@@ -1,14 +1,4 @@
 namespace HappyHour
 {
-    public class AppBootstrapper : Cocktail.FrameworkBootstrapper<ViewModels.MainPageViewModel>
-    {
-        // Needed if the ListBox displays DrinkOrderEntityViews
-        protected override void Configure()
-        {
-            base.Configure();
-            Caliburn.Micro.ViewLocator.NameTransformer
-                // Map "Model" classes to views in "HappyHour.Views"
-                .AddRule(@"^Model\.(\w*)", "HappyHour.Views.${1}EntityView");
-        }
-    }
+    public class AppBootstrapper : Cocktail.FrameworkBootstrapper<ViewModels.MainPageViewModel> { }
 }

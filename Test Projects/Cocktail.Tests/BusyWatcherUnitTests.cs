3a085d0a5ff4a2d8871d6a5de4b2e014ca2f6378
Marcel Good <marcelg@ideablade.com>
2012-01-31 00:44:04 +0000

    Added unit tests for BusyWatcher.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@199 3d6527be-44ab-9e4e-9680-169f2067cef8

---
+++ Test Projects/Cocktail.Tests/BusyWatcherUnitTests.cs
@@ -0,0 +1,65 @@
+﻿using Cocktail.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cocktail.Tests
+{
+    [TestClass]
+    public class BusyWatcherUnitTests : CocktailTestBase
+    {
+        [TestMethod]
+        public void ShouldBeBusyInsideScope()
+        {
+            var busyWatcher = new BusyWatcher();
+            using (busyWatcher.GetTicket())
+            {
+                Assert.IsTrue(busyWatcher.IsBusy);
+            }
+
+            Assert.IsFalse(busyWatcher.IsBusy);
+        }
+
+        [TestMethod]
+        public void ShouldNotifyOfPropertyChanged()
+        {
+            int propertyChangedCount = 0;
+            var busyWatcher = new BusyWatcher();
+            busyWatcher.PropertyChanged += (sender, args) => propertyChangedCount++;
+
+            using (busyWatcher.GetTicket())
+            {
+                Assert.IsTrue(propertyChangedCount == 1);
+            }
+
+            Assert.IsTrue(propertyChangedCount == 2);
+        }
+
+        [TestMethod]
+        public void ShouldIncrementDecrementBusyCounter()
+        {
+            var busyWatcher = new BusyWatcher();
+            busyWatcher.AddWatch();
+            Assert.IsTrue(busyWatcher.IsBusy);
+
+            busyWatcher.RemoveWatch();
+            Assert.IsFalse(busyWatcher.IsBusy);
+        }
+
+        [TestMethod]
+        public void ShouldNestBusyState()
+        {
+            var busyWatcher = new BusyWatcher();
+
+            busyWatcher.AddWatch();
+            Assert.IsTrue(busyWatcher.IsBusy);
+
+            using (busyWatcher.GetTicket())
+            {
+                Assert.IsTrue(busyWatcher.IsBusy);
+            }
+
+            Assert.IsTrue(busyWatcher.IsBusy);
+            busyWatcher.RemoveWatch();
+            Assert.IsFalse(busyWatcher.IsBusy);
+        }
+    }
+}

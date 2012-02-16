f756fbab75c2bcf0d556ad8a93493bca6f83cf30
Marcel Good <marcelg@ideablade.com>
2012-02-16 07:19:49 +0000

    Updated CocktailHelpReference.chm.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@404 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/Properties/AssemblyInfo.cs
+++ Cocktail/Properties/AssemblyInfo.cs
@@ -92,5 +92,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.0.395")]
+[assembly: AssemblyVersion("0.3.0.403")]
 

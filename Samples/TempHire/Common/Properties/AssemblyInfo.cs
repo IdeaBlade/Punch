5242077a968d7b4f0660ebadd8d016d90550e63f
Marcel Good <marcelg@ideablade.com>
2012-02-18 16:31:06 +0000

    Modified output paths and assembly info to support side-by-side builds.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/DEV_TempHire_WPF@434 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Samples/TempHire/Common/Properties/AssemblyInfo.cs
+++ Samples/TempHire/Common/Properties/AssemblyInfo.cs
@@ -5,20 +5,26 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if SILVERLIGHT
 [assembly: AssemblyTitle("Common.SL")]
+#else
+[assembly: AssemblyTitle("Common")]
+#endif
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Common.SL")]
+[assembly: AssemblyProduct("TempHire")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if SILVERLIGHT
 [assembly: InternalsVisibleTo("IdeaBlade.Core.SL, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3f302890eb528"
                                                      + "1a7ab39b936ad9e0eded7c4a41abb440bead71ff5a31d51e865606b2a7e6d0b9dd0d92b113b9d1"
                                                      + "0fb13f01fb5d856e99c1e61777cf4772d29bad7e66ffb93fc5cbd63b395046c06ff57db6ecbeee"
                                                      + "4bdd6effc405878d65cfc4911708ed650da935d733fc5dc707f74910e025ac080543e01a6cc863"
                                                      + "b9f85ffc")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

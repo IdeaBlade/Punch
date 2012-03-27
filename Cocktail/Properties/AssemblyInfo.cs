353c4d957ece089c84400adce1c189aba53342e3
Marcel Good <marcelg@ideablade.com>
2012-03-27 03:12:04 +0000

    Breaking Change: Removed ConnectionOptions.CompositionContextName.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@565 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/Properties/AssemblyInfo.cs
+++ Cocktail/Properties/AssemblyInfo.cs
@@ -87,5 +87,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.0.546")]
+[assembly: AssemblyVersion("0.5.0.565")]
 

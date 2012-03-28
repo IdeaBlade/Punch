df1817b2594d1c64a82a9d07b416c642a8bab0c3
Marcel Good <marcelg@ideablade.com>
2012-03-28 19:14:01 +0000

    Improved Harness to better cope with large amounts of ViewModels.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@571 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/HarnessViewModel.cs
+++ Cocktail/HarnessViewModel.cs
@@ -1,14 +1,14 @@
-﻿//====================================================================================================================
-// Copyright (c) 2012 IdeaBlade
-//====================================================================================================================
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
-// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
-// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
-//====================================================================================================================
-// USE OF THIS SOFTWARE IS GOVERENED BY THE LICENSING TERMS WHICH CAN BE FOUND AT
-// http://cocktail.ideablade.com/licensing
-//====================================================================================================================
+﻿// ====================================================================================================================
+//   Copyright (c) 2012 IdeaBlade
+// ====================================================================================================================
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+//   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+// ====================================================================================================================
+//   USE OF THIS SOFTWARE IS GOVERENED BY THE LICENSING TERMS WHICH CAN BE FOUND AT
+//   http://cocktail.ideablade.com/licensing
+// ====================================================================================================================
 
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
@@ -18,22 +18,27 @@ using IdeaBlade.Core;
 
 namespace Cocktail
 {
-    /// <summary>ViewModel implementing the Development Harness. Specify this ViewModel as the TRootModel in the Application Bootstrapper to create a Development
-    /// Harness</summary>
+    /// <summary>
+    ///   ViewModel implementing the Development Harness. Specify this ViewModel as the TRootModel in the Application Bootstrapper to create a Development Harness
+    /// </summary>
     /// <example>
-    /// 	<code title="Development Harness Bootstrapper" description="Demonstrates how to specify the HarnessView model in the Application Bootstrapper to create a Development Harness." lang="CS">
-    /// public class AppBootstrapper : FrameworkBootstrapper&lt;HarnessViewModel&gt;
-    /// {
+    ///   <code title="Development Harness Bootstrapper"
+    ///     description="Demonstrates how to specify the HarnessView model in the Application Bootstrapper to create a Development Harness."
+    ///     lang="CS">public class AppBootstrapper : FrameworkBootstrapper&lt;HarnessViewModel&gt;
+    ///     {
     ///     // Additional code
-    /// }</code>
+    ///     }</code>
     /// </example>
     [Export]
     public class HarnessViewModel : Conductor<object>
     {
         private readonly Dictionary<string, object> _viewModels;
+        private string _activeName;
 
-        /// <summary>Initializes a new instance.</summary>
-        /// <param name="viewModels">The list of discovered ViewModels injected through MEF.</param>
+        /// <summary>
+        ///   Initializes a new instance.
+        /// </summary>
+        /// <param name="viewModels"> The list of discovered ViewModels injected through MEF. </param>
         [ImportingConstructor]
         public HarnessViewModel([ImportMany] IEnumerable<IDiscoverableViewModel> viewModels)
         {
@@ -43,13 +48,30 @@ namespace Cocktail
             EventFns.Subscribe(this);
         }
 
-        /// <summary>Bindable collection exposing the names of all discovered ViewModels.</summary>
+        /// <summary>
+        ///   Bindable collection exposing the names of all discovered ViewModels.
+        /// </summary>
         public BindableCollection<string> Names
         {
             get { return new BindableCollection<string>(_viewModels.Keys.OrderBy(k => k)); }
         }
 
-        /// <summary>Activates the ViewModel with the given name.</summary>
+        /// <summary>
+        ///   Returns the name of the current active view model.
+        /// </summary>
+        public string ActiveName
+        {
+            get { return _activeName; }
+            set
+            {
+                _activeName = value;
+                NotifyOfPropertyChange(() => ActiveName);
+            }
+        }
+
+        /// <summary>
+        ///   Activates the ViewModel with the given name.
+        /// </summary>
         public void ActivateViewModel(string name)
         {
             object viewModel;
@@ -60,6 +82,7 @@ namespace Cocktail
                     harnessAware.Setup();
 
                 ActivateItem(viewModel);
+                ActiveName = name;
             }
         }
     }

cbc9fe51979730bfd64066bbf50bc03d58feda58
Marcel Good <marcelg@ideablade.com>
2012-04-30 07:01:57 +0000

    Updated revision #.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@689 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/Properties/AssemblyInfo.cs
+++ Cocktail/Properties/AssemblyInfo.cs
@@ -87,5 +87,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.0.664")]
+[assembly: AssemblyVersion("0.6.0.676")]
 

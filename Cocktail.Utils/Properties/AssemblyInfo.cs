57612849a97d1bb0cc9fe55d9e458c01124b117a
Marcel Good <marcelg@ideablade.com>
2012-07-26 04:51:44 +0000

    Updated help and rebuilt NuGet package.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@800 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail.Utils/Properties/AssemblyInfo.cs
+++ Cocktail.Utils/Properties/AssemblyInfo.cs
@@ -62,4 +62,4 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.8.0.795")]
+[assembly: AssemblyVersion("0.8.0.800")]
--- Cocktail/Properties/AssemblyInfo.cs
+++ Cocktail/Properties/AssemblyInfo.cs
@@ -87,5 +87,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.0.795")]
+[assembly: AssemblyVersion("0.8.0.800")]
 

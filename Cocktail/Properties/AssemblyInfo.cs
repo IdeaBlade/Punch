7b9df2f4bde811facbe8a170af345653dd41e876
Marcel Good <marcelg@ideablade.com>
2012-08-20 02:28:59 +0000

    Create WinRT unit test project.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/Cocktail_V2@950 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/Properties/AssemblyInfo.cs
+++ Cocktail/Properties/AssemblyInfo.cs
@@ -53,6 +53,13 @@ using System.Windows.Markup;
                                                      + "0fb13f01fb5d856e99c1e61777cf4772d29bad7e66ffb93fc5cbd63b395046c06ff57db6ecbeee"
                                                      + "4bdd6effc405878d65cfc4911708ed650da935d733fc5dc707f74910e025ac080543e01a6cc863"
                                                      + "b9f85ffc")]
+#elif WinRT
+[assembly: InternalsVisibleTo("Cocktail.Tests.WinRT")]
+[assembly: InternalsVisibleTo("IdeaBlade.Core.Metro, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3f302890eb528"
+                                                     + "1a7ab39b936ad9e0eded7c4a41abb440bead71ff5a31d51e865606b2a7e6d0b9dd0d92b113b9d1"
+                                                     + "0fb13f01fb5d856e99c1e61777cf4772d29bad7e66ffb93fc5cbd63b395046c06ff57db6ecbeee"
+                                                     + "4bdd6effc405878d65cfc4911708ed650da935d733fc5dc707f74910e025ac080543e01a6cc863"
+                                                     + "b9f85ffc")]
 #else
 [assembly: InternalsVisibleTo("Cocktail.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000711e4133e5919"
                                                      + "6b877f67b04088121b1d6c3a25cb8791422c56986774125d0b805da768e11c38bbe2a395a98611"

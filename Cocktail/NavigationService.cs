6d3a5d8b89cd6f2cae123c2280deaecddd089be2
Marcel Good <marcelg@ideablade.com>
2012-08-27 03:35:10 +0000

    Hide object members.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/Cocktail_V2@985 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/NavigationService.cs
+++ Cocktail/NavigationService.cs
@@ -44,7 +44,7 @@ namespace Cocktail
     /// <summary>
     ///   A service that implements UI navigation logic.
     /// </summary>
-    public partial interface INavigationService
+    public partial interface INavigationService : IHideObjectMembers
     {
         /// <summary>
         ///   Returns the current active ViewModel or null.

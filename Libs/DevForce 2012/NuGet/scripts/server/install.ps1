6a443405aa66913e896c3be41daf0dcd501b51a9
Marcel Good <marcelg@ideablade.com>
2012-08-31 01:05:04 +0000

    Simplified NuGet packages and added transformations.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/Cocktail_V2@1050 3d6527be-44ab-9e4e-9680-169f2067cef8

---
+++ Libs/DevForce 2012/NuGet/scripts/server/install.ps1
@@ -0,0 +1,10 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+#$project = Get-Project   # for testing
+
+#if this is a web application, add IdeaBlade.EntityModel.Web.dll reference
+if ($project.ExtenderNames.Contains("WebApplication"))
+{
+    $assembly = $installPath + "\lib\net45\IdeaBlade.EntityModel.Web.dll"
+    $project.Object.References.Add($assembly)
+}
---
+++ Libs/DevForce 2012/NuGet/scripts/server/uninstall.ps1
@@ -0,0 +1,3 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project.Object.References | Where-Object { $_.Name -eq 'IdeaBlade.EntityModel.Web' } | ForEach-Object { $_.Remove() }

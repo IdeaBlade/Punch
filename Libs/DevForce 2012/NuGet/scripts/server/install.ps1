9bac1e08a9e3ee7d9ba73b2cd7ffccfc97fa0e4a
Marcel Good <marcelg@ideablade.com>
2012-08-31 01:25:07 +0000

    Remove Global.asax if project is not a web application.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/Cocktail_V2@1051 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Libs/DevForce 2012/NuGet/scripts/server/install.ps1
+++ Libs/DevForce 2012/NuGet/scripts/server/install.ps1
@@ -2,9 +2,14 @@
 
 #$project = Get-Project   # for testing
 
-#if this is a web application, add IdeaBlade.EntityModel.Web.dll reference
 if ($project.ExtenderNames.Contains("WebApplication"))
 {
+    #if this is a web application, add IdeaBlade.EntityModel.Web.dll reference
     $assembly = $installPath + "\lib\net45\IdeaBlade.EntityModel.Web.dll"
     $project.Object.References.Add($assembly)
+}
+else
+{
+    #if this is not a web application, remove Global.asax
+    $project.ProjectItems | Where-Object { $_.Name -eq 'Global.asax' } | ForEach-Object { $_.Remove() }
 }

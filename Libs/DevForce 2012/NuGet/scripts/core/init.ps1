ac2b758d374afe7b0144ea1939db21aab6963d69
Marcel Good <marcelg@ideablade.com>
2012-08-31 18:29:58 +0000

    Updated vsixes and add prompt at the end to restart VS.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/branches/Cocktail_V2@1058 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Libs/DevForce 2012/NuGet/scripts/core/init.ps1
+++ Libs/DevForce 2012/NuGet/scripts/core/init.ps1
@@ -1,9 +1,9 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-#Get the first project
+# Get the first project. $project parameter is null in init scripts
 $project = ([Array] (Get-Project -All))[0]
 
-#The following three constants must be updated to reflect the correct versions
+# The following three constants must be updated to reflect the correct versions
 $extensionId = "DF2012_IdeaBladeDesigner,7.0.0.0"
 $extensionVsix = "IdeaBlade.VisualStudio.OM.Designer.11.0.vsix"
 $templatesVsix = "IdeaBlade.VisualStudio.TemplateInstaller.vsix"
@@ -29,8 +29,14 @@ if (!$extensionTypes -or !$extensionTypes.GetValue($extensionId))
     $process.WaitForExit()
     $process = [diagnostics.process]::Start($installer, "`"$templatesVsixPath`"")
     $process.WaitForExit()
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [Windows.Forms.MessageBox]::Show("You must restart Microsoft Visual Studio in order for the changes to take effect.", 
+        "Extensions and Updates", [Windows.Forms.MessageBoxButtons]::OK, 
+        [System.Windows.Forms.MessageBoxIcon]::Information) | Out-Null
+
 }
 else
 {
-    { "DevForce 2012 EDM Designer Extension is already installed" }
+    Write-Output "DevForce 2012 EDMX Extension is already installed."
 }

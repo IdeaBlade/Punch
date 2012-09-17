b58c372a1bea409073e11df1f29eb186077adfe2
Marcel Good <marcelg@ideablade.com>
2012-09-17 15:45:26 +0000

    Dropped build number from the assembly version and rebuilt NuGet packages.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@1116 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail.Compat/Properties/AssemblyInfo.cs
+++ Cocktail.Compat/Properties/AssemblyInfo.cs
@@ -58,4 +58,4 @@ using System.Windows.Markup;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.1113")]
+[assembly: AssemblyVersion("2.0.0.0")]

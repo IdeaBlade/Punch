26b855dc8d98b722b9eb3b481d91b961c5e37f02
Marcel Good <marcelg@ideablade.com>
2012-09-29 10:16:07 +0000

    Remove obsolete method.
    git-svn-id: http://profserv.ideablade.com:81/svn/Cocktail/trunk@1128 3d6527be-44ab-9e4e-9680-169f2067cef8

--- Cocktail/MefCompositionProvider.DevForce.cs
+++ Cocktail/MefCompositionProvider.DevForce.cs
@@ -19,27 +19,6 @@ namespace Cocktail
 {
     internal partial class MefCompositionProvider
     {
-#if !SILVERLIGHT5
-
-        /// <summary>
-        /// Enables full design time support for the specified EntityManager type.
-        /// </summary>
-        /// <typeparam name="T">The type of EntityManager needing design time support.</typeparam>
-        /// <remarks>This method must be called as early as possible, usually in the bootstrapper's static constructor.</remarks>
-        public static void EnableDesignTimeSupport<T>() where T : EntityManager
-        {
-            if (DesignTime.InDesignMode())
-            {
-                string assemblyName = typeof(T).GetAssembly().FullName;
-                if (IdeaBladeConfig.Instance.ProbeAssemblyNames.Contains(assemblyName))
-                    return;
-
-                IdeaBladeConfig.Instance.ProbeAssemblyNames.Add(assemblyName);
-            }
-        }
-
-#endif
-
 #if !NETFX_CORE
         internal static void EnsureRequiredProbeAssemblies()
         {

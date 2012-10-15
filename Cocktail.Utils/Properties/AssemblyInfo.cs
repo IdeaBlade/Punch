6760b048bc6962eb90dca86ab3d076e303bf41ec
Marcel Good <marcelg@ideablade.com>
2012-10-15 12:23:55 -0700

    Update branch to v1.0.2

--- Cocktail.Utils/Properties/AssemblyInfo.cs
+++ Cocktail.Utils/Properties/AssemblyInfo.cs
@@ -62,4 +62,4 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.1.918")]
+[assembly: AssemblyVersion("1.0.2.0")]

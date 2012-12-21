a959ebcf4a2e7cd6787237d0a4077e10dd883f27
Marcel Good <marcelg@ideablade.com>
2012-12-21 11:52:36 -0800

    Update HappyHour to Cocktail v2.2 and DevForce v7.0.3

---
+++ Samples/HappyHour/UpdatePackages.ps1
@@ -0,0 +1,20 @@
+﻿foreach ($solution in dir -Path .\ -Filter "*.sln" -Recurse)
+{
+    $solutionDir = [IO.Path]::GetDirectoryName($solution.FullName)
+    $packages = [IO.Path]::Combine($solutionDir, "packages")
+
+    # Restore packages
+    foreach ($pkg in dir -Path $solutionDir -Filter "packages.config" -Recurse)
+    {
+        nuget install $pkg.FullName -OutputDirectory $packages
+    }
+
+    # Update packages
+    nuget update $solution.FullName
+
+    # Delete packages folder. It will be restored on next build
+    if (Test-Path -Path $packages)
+    {
+        del $packages -Recurse
+    }
+}

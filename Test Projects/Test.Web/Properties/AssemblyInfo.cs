53a4244fc21216da9391946722ce8e3bdb8394f6
Marcel Good <marcelg@ideablade.com>
2014-07-31 10:46:54 -0700

    Update to DevForce v7.2.4

--- Test Projects/Test.Web/Properties/AssemblyInfo.cs
+++ Test Projects/Test.Web/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.4")]
-[assembly: AssemblyFileVersion("2.4.4")]
+[assembly: AssemblyVersion("2.4.5")]
+[assembly: AssemblyFileVersion("2.4.5")]

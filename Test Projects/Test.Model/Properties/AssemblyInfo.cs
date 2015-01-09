f9aca23c0eab6fc19e481d7a50c9b1f11b97f044
Marcel Good <marcelg@ideablade.com>
2015-01-09 12:07:08 -0800

    Update to DevForce v7.2.5

--- Test Projects/Test.Model/Properties/AssemblyInfo.cs
+++ Test Projects/Test.Model/Properties/AssemblyInfo.cs
@@ -62,6 +62,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.5")]
-[assembly: AssemblyFileVersion("2.4.5")]
+[assembly: AssemblyVersion("2.4.6")]
+[assembly: AssemblyFileVersion("2.4.6")]
 [assembly: InternalsVisibleTo("System.Runtime.Serialization, PublicKey=00240000048000009400000006020000002400005253413100040000010001008d56c76f9e8649383049f383c44be0ec204181822a6c31cf5eb7ef486944d032188ea1d3920763712ccb12d75fb77e9811149e6148e5d32fbaab37611c1878ddc19e20ef135d0cb2cff2bfec3d115810c3d9069638fe4be215dbf795861920e5ab6f7db2e2ceef136ac23d5dd2bf031700aec232f6c6b1c785b4305c123b37ab")]
